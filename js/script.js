6f9c82ee07d8f7a2838fb0b3b12273640f20ef86
vramos29 <vramos1@northpark.edu>
2026-03-20 19:35:36 -0500

    added object to track the user data to eventually calculate their score and give them their answer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 console.log("script.js connected!");
 
 
-let aCounter = 0;
-let bCounter = 0;
-let cCounter = 0;
-let dCounter = 0;
+let userAnswers = {}
 
 let buttons = document.querySelectorAll("#questions button");
 
@@ -16,17 +13,29 @@ buttons.forEach(function(button){
 
         button.classList.add("selected");
 
-        let answer = button.getAttribute("data-answer");
+        //let answer = button.getAttribute("data-answer");
         
-        if (answer === "A") {
-            aCounter += 1;
-        } else if (answer === "B") {
-            bCounter += 1;
-        } else if (answer === "C") {
-            cCounter += 1;
-        } else if (answer === "D") {
-            dCounter += 1;
-        }
+        let answer = button.dataset.answer
+        let response = button.dataset.answer;
+        userAnswers[answer] = response;
+        console.log(userAnswers);
         
     })
 })
+
+/*
+if ("A" in userAnswers) {
+    let finalAnswer = "You are Glacier National Park!";
+    document.getElementById("result-text").innerHTML = finalAnswer;
+} else if ("B" in userAnswers >= 2) {
+    let finalAnswer = "You are Badlands National Park!";
+    document.getElementById("result-text").innerHTML = finalAnswer;
+} else if ("C" in userAnswers >= 2) {
+    let finalAnswer = "You are Olympic National Park!";
+    document.getElementById("result-text").innerHTML = finalAnswer;
+} else if ("D" in userAnswers >= 2) {
+    let finalAnswer = "You are Grand Teton National Park!";
+    document.getElementById("result-text").innerHTML = finalAnswer;
+}
+
+*/
