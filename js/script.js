231c6675207b309b0a14fff83736b8d09c86f819
vramos29 <vramos1@northpark.edu>
2026-03-22 09:44:18 -0500

    solved answer problem, now will figure out how to display results at the end

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 console.log("script.js connected!");
 
 
-let userAnswers = {}
-
 let buttons = document.querySelectorAll("#questions button");
 
+let userAnswers = {}
 buttons.forEach(function(button){
     button.addEventListener("click", function() {
         buttons.forEach(function(btn) {
@@ -13,29 +12,79 @@ buttons.forEach(function(button){
 
         button.classList.add("selected");
 
-        //let answer = button.getAttribute("data-answer");
+        let answer = button.getAttribute("data-answer");
+        let output = document.getElementById("question-1");
+
+        if (answer === "A1") {
+            output.textContent = "Question 1 Answer: A";
+        } else if (answer === "B1") {
+            output.textContent = "Question 1 Answer: B";
+        } else if (answer === "C1") {
+            output.textContent = "Question 1 Answer: C";
+        } else if (answer === "D1") {
+            output.textContent = "Question 1 Answer: D";
+        };
         
-        let answer = button.dataset.answer
+        
+        let output2 = document.getElementById("question-2");
+
+        if (answer === "A2") {
+            output2.textContent = "Question 2 Answer: A";
+        } else if (answer === "B2") {
+            output2.textContent = "Question 2 Answer: B";
+        } else if (answer === "C2") {
+            output2.textContent = "Question 2 Answer: C";
+        } else if (answer === "D2") {
+            output2.textContent = "Question 2 Answer: D";
+        };
+
+
+        let output3 = document.getElementById("question-3");
+
+        if (answer === "A3") {
+            output3.textContent = "Question 3 Answer: A";
+        } else if (answer === "B3") {
+            output3.textContent = "Question 3 Answer: B";
+        } else if (answer === "C3") {
+            output3.textContent = "Question 3 Answer: C";
+        } else if (answer === "D3") {
+            output3.textContent = "Question 3 Answer: D";
+        };
+
+
+        let output4 = document.getElementById("question-4");
+
+        if (answer === "A4") {
+            output4.textContent = "Question 4 Answer: A";
+        } else if (answer === "B4") {
+            output4.textContent = "Question 4 Answer: B";
+        } else if (answer === "C4") {
+            output4.textContent = "Question 4 Answer: C";
+        } else if (answer === "D4") {
+            output4.textContent = "Question 4 Answer: D";
+        };
+
+
+        
+        let answers = button.dataset.answer
         let response = button.dataset.answer;
-        userAnswers[answer] = response;
+        userAnswers[answers] = response;
         console.log(userAnswers);
         
-    })
-})
+    });
+});
 
-/*
-if ("A" in userAnswers) {
-    let finalAnswer = "You are Glacier National Park!";
-    document.getElementById("result-text").innerHTML = finalAnswer;
-} else if ("B" in userAnswers >= 2) {
-    let finalAnswer = "You are Badlands National Park!";
-    document.getElementById("result-text").innerHTML = finalAnswer;
-} else if ("C" in userAnswers >= 2) {
-    let finalAnswer = "You are Olympic National Park!";
-    document.getElementById("result-text").innerHTML = finalAnswer;
-} else if ("D" in userAnswers >= 2) {
-    let finalAnswer = "You are Grand Teton National Park!";
-    document.getElementById("result-text").innerHTML = finalAnswer;
+for (let key in userAnswers) {
+    if (typeof userAnswers[key] === "string") {
+        let newString = "";
+        for (let char of userAnswers[key]) {
+            if (char < "0" || char > "9") {
+                newString += char;
+            } 
+        }
+        userAnswers[key] = newString
+    }
 }
 
-*/
+console.log(userAnswers)
+
